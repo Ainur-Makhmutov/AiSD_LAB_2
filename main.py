09e12e3af4b3ee048b5b637d3a9df25c72d83913
Ainur-Makhmutov <99477737+Ainur-Makhmutov@users.noreply.github.com>
2022-03-04 09:57:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+n, n_t, k, k_t = 0, 0, 0, 0
+array = []
 
+with open("C:/python/laba_2/in.txt", "r") as f:
+    array = f.read().split()
+
+for i in range(0, len(array)):
+    if int(array[i]) % 2 == 0:
+        if k_t == 0:
+            n_t += 1
+            k_t = i
+        else:
+            n_t += 1
+        n = n_t
+        k = k_t
+    else:
+        n_t = 0
+        k_t = 0
+
+
+if n != 0:
+    del array[k+n:]
+    del array[0:k]
+
+    print ("Массив: ", array)
+    print ("Длинна: ", n)
+    print ("Позиция: ", k)
+else:
+    print ("В данном файле четных последовательностей нет")
